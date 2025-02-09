c26177edf8133923f0df873e2cea0034241f75a9
Vini <vini.ba.dev@gmail.com>
2025-02-09 20:39:27 -0300

    add Mocks for tests

---
+++ src/Mocks.ts
@@ -0,0 +1,62 @@
+export const users = [
+  {
+    name: "Alice Johnson",
+    age: 25,
+    email: "alice.johnson@example.com",
+    type: "admin",
+  },
+  {
+    name: "Bob Smith",
+    age: 30,
+    email: "bob.smith@example.com",
+    type: "common",
+  },
+  {
+    name: "Charlie Brown",
+    age: 40,
+    email: "charlie.brown@example.com",
+    type: "admin",
+  },
+  {
+    name: "Diana Prince",
+    age: 30,
+    email: "diana.prince@example.com",
+    type: "common",
+  },
+  {
+    name: "Evan Wright",
+    age: 35,
+    email: "evan.wright@example.com",
+    type: "admin",
+  },
+  {
+    name: "Fiona Green",
+    age: 29,
+    email: "fiona.green@example.com",
+    type: "common",
+  },
+  {
+    name: "George Hill",
+    age: 50,
+    email: "george.hill@example.com",
+    type: "admin",
+  },
+  {
+    name: "Hannah Scott",
+    age: 19,
+    email: "hannah.scott@example.com",
+    type: "common",
+  },
+  {
+    name: "Ian Clarke",
+    age: 28,
+    email: "ian.clarke@example.com",
+    type: "admin",
+  },
+  {
+    name: "Julia Adams",
+    age: 45,
+    email: "julia.adams@example.com",
+    type: "common",
+  },
+];
