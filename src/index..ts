176f749afbb884e52a375c27b4c4760ee7621e14
Vini <vini.ba.dev@gmail.com>
2025-02-09 14:25:15 -0300

    Testing

--- src/index..ts
+++ src/index..ts
@@ -1,1 +1,24 @@
-console.log('Hello world');
+import { Db } from "./Db";
+import { z } from "zod";
+
+const UserSchema = z.object({
+  name: z.string(),
+  age: z.number(),
+});
+
+const client = new Db();
+
+client.CreateATable("users", UserSchema);
+
+client.tables.users.Create({
+  age: 18,
+  name: "Vini",
+});
+
+const t = client.tables.users.FindUnique({
+  where: {
+    name: "Vini",
+  },
+});
+
+console.log(t);
